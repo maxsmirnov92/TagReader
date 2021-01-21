965d8a0c25e408a42fdd8bf24beb093768506ca7
maxsmirnov92 <max.smirnov92@yandex.ru>
2021-01-21 23:17:46 +0400

    skip invalid mp3s, logging added

--- tagreader/src/main/kotlin/net/maxsmr/tagreader/TagReader.kt
+++ tagreader/src/main/kotlin/net/maxsmr/tagreader/TagReader.kt
@@ -2,6 +2,8 @@ package net.maxsmr.tagreader
 
 import com.mpatric.mp3agic.Mp3File
 import net.maxsmr.commonutils.data.*
+import net.maxsmr.commonutils.data.text.EMPTY_STRING
+import net.maxsmr.commonutils.data.text.trimWithCondition
 import net.maxsmr.commonutils.logger.BaseLogger
 import net.maxsmr.commonutils.logger.SimpleSystemLogger
 import net.maxsmr.commonutils.logger.holder.BaseLoggerHolder
@@ -11,29 +13,50 @@ import java.util.concurrent.TimeUnit
 private const val EXT_MP3 = "mp3"
 private const val PATTERN_TAG_FORMAT = "%s\t%s\t%s\t%d\t%d\tL\t%d\t"
 
+private val argsNames = arrayOf("-rootDir", "-recursive", "-targetDir", "-targetFile", "-append", "-header", "-log")
+
 fun main(args: Array<String>) {
 
-    BaseLoggerHolder.initInstance {
-        object : BaseLoggerHolder(false) {
-            override fun createLogger(className: String): BaseLogger {
-                return SimpleSystemLogger(className)
-            }
-        }
-    }
-
-    val argsNames = arrayOf("-rootDir", "-recursive", "-targetDir", "-targetFile", "-append", "-header")
+    fun CharSequence?.trimEndSeparator() = this?.let {
+        trimWithCondition(
+                it,
+                false,
+                File.separatorChar,
+                CompareCondition.EQUAL
+        ).toString()
+    } ?: EMPTY_STRING
 
     val argsParser = ArgsParser(*argsNames)
     argsParser.setArgs(*args)
 
+    val isLoggingEnabled = argsParser.containsArg(6, true)
+
+    BaseLoggerHolder.initInstance {
+        object : BaseLoggerHolder(true) {
+            override fun createLogger(className: String): BaseLogger? {
+                return if (isLoggingEnabled) {
+                    SimpleSystemLogger(className)
+                } else {
+                    null
+                }
+            }
+        }
+    }
+
+    val logger = BaseLoggerHolder.getInstance().getLogger<BaseLogger>("TagReader")
+
+
     val rootDir = argsParser.findPairArg(0, true)?.let {
-        File(it)
+        File(it.trimEndSeparator())
     }
 
     val recursive = argsParser.containsArg(1, true)
 
     val append = argsParser.containsArg(4, true)
-    val targetTextFile = createFileOrThrow(argsParser.findPairArg(3, true), argsParser.findPairArg(2, true), !append)
+    val targetTextFile = createFileOrThrow(
+            argsParser.findPairArg(3, true).trimEndSeparator(),
+            argsParser.findPairArg(2, true).trimEndSeparator(),
+            !append)
 
     val header = argsParser.findPairArg(5, true)
 
@@ -42,6 +65,7 @@ fun main(args: Array<String>) {
     }
 
     if (!isFileValidOrThrow(targetTextFile) && header != null) {
+        logger.d("Writing header to '$targetTextFile'...")
         writeStringsToFileOrThrow(targetTextFile, listOf(header.replace("\\n", System.lineSeparator())), false)
     }
 
@@ -55,27 +79,38 @@ fun main(args: Array<String>) {
 
     val currentTimestamp = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis())
 
-    files.forEach {
+    files.forEach loop@ {
+        logger.i("")
+        logger.i("Scanning file '$it'...")
+
         val mp3File = Mp3File(it)
 
         val duration = mp3File.lengthInSeconds
-        var artist: String? = null
-        var album: String? = null
-        var title: String? = null
-        var mergedTrackNumber: String? = null
-        var trackNumber: Int? = null
+        val artist: String?
+        val album: String?
+        val title: String?
+        val mergedTrackNumber: String?
 
-        if (mp3File.hasId3v2Tag()) {
-            artist = mp3File.id3v2Tag.artist
-            album = mp3File.id3v2Tag.album
-            title = mp3File.id3v2Tag.title
-            mergedTrackNumber = mp3File.id3v2Tag.track
-        } else if (mp3File.hasId3v1Tag()) {
-            artist = mp3File.id3v1Tag.artist
-            album = mp3File.id3v1Tag.album
-            title = mp3File.id3v1Tag.title
-            mergedTrackNumber = mp3File.id3v1Tag.track
+        when {
+            mp3File.hasId3v2Tag() -> {
+                artist = mp3File.id3v2Tag.artist
+                album = mp3File.id3v2Tag.album
+                title = mp3File.id3v2Tag.title
+                mergedTrackNumber = mp3File.id3v2Tag.track
+            }
+            mp3File.hasId3v1Tag() -> {
+                artist = mp3File.id3v1Tag.artist
+                album = mp3File.id3v1Tag.album
+                title = mp3File.id3v1Tag.title
+                mergedTrackNumber = mp3File.id3v1Tag.track
+            }
+            else -> {
+                logger.e("Invalid mp3 file: '$mp3File' (no ID3s)")
+                return@loop
+            }
         }
+
+        var trackNumber: Int? = null
 
         mergedTrackNumber?.let {
             val parts = mergedTrackNumber.split("/")
@@ -84,7 +119,14 @@ fun main(args: Array<String>) {
             }
         }
 
-        val scrobblerString = PATTERN_TAG_FORMAT.format(artist, album, title, trackNumber, duration, currentTimestamp)
-        writeStringsToFileOrThrow(targetTextFile, listOf(scrobblerString), append)
+        val scrobblerString = PATTERN_TAG_FORMAT.format(
+                artist ?: EMPTY_STRING,
+                album ?: EMPTY_STRING,
+                title ?: EMPTY_STRING,
+                trackNumber ?: EMPTY_STRING,
+                duration,
+                currentTimestamp
+        )
+        writeStringsToFileOrThrow(targetTextFile, listOf(scrobblerString), true)
     }
 }
